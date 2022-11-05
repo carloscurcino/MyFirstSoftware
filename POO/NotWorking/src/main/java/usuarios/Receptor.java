2dec18a6997bda9088e82af98a89402c263dd0d7
carloscurcino <carlosdanielf541@gmail.com>
2022-11-04 22:06:15 -0300

    Conexão com o banco de dados, 100% de sucesso, exqueça tudo, só agradece!

---
+++ POO/NotWorking/src/main/java/usuarios/Receptor.java
@@ -0,0 +1,22 @@
+package usuarios;
+
+public class Receptor 
+{
+	private String name;
+	private int matricul;
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	public int getMatricul(){
+		return this.matricul;
+	}
+	
+	public void setMatricul(int M) {
+		this.matricul = M;
+	}
+}
